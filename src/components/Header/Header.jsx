9804b246f0e0193238d4c9ee502c6f19905c3bf6
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-02-18 23:46:45 +0100

    Add logo name and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
+import { NavLink } from "react-router";
 import NavBar from "../NavBar/NavBar";
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className="header">
+    <header className="header">
+      <div className="logo">
+        <NavLink to="/">
+          upTo20
+        </NavLink>
+      </div>
       <h3>Wybierz działanie matematyczne</h3>
       <NavBar />
-    </div>
+    </header>
   )
 }
 
