faf9242c02a5dff4d0ab0ae641f4a03cbd2b9792
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-02-23 23:40:06 +0100

    Add 'range' input in Addition.js

--- src/components/Addition/Addition.jsx
+++ src/components/Addition/Addition.jsx
@@ -4,19 +4,19 @@ import Calculation from '../Calculation/Calculation';
 
 const Addition = () => {
   const inputsRef = useRef([]);
-  // const [range, setRange] = useState(11);
+  const [range, setRange] = useState(11);
 
-  // const handleChange = (e) => {
-  //   setRange(e.target.value)
-  // }
+  const handleChange = (e) => {
+    setRange(e.target.value)
+  }
 
   return (
    <div className='operation-container'>
       <h2>Dodawanie</h2>
-      {/* <h3>Wybierz zakres</h3>
-      <input type="number" onChange={handleChange}></input> */}
-      {[...Array(3)].map((_, index) => (
-        <Calculation key={index} operation={'add'} index={index} inputsRef={inputsRef} /*range={range} *//>
+      <h3>Wybierz zakres</h3>
+      <input type="number" onChange={handleChange}></input>
+      {[...Array(5)].map((_, index) => (
+        <Calculation key={index} operation={'add'} index={index} inputsRef={inputsRef} range={range} />
       ))}
     </div>
   )
